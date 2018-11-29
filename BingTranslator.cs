c63d2f8b9cc45be59497d92ec92474bffa7066b2
Anduin2017 <anduin@aiursoft.com>
2018-11-30 01:12:25 +0800

    FIrst version.

---
+++ BingTranslator.cs
@@ -0,0 +1,76 @@
+﻿#pragma warning disable IDE1006 // Naming Styles
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CoreTranslator
+{
+    public class DetectedLanguage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string language { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double score { get; set; }
+    }
+
+    public class TranslationsItem
+    {
+        /// <summary>
+        /// 我可以。
+        /// </summary>
+        public string text { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string to { get; set; }
+    }
+
+    public class W
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DetectedLanguage detectedLanguage { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<TranslationsItem> translations { get; set; }
+    }
+
+    public class Translation
+    {
+        public string Text { get; set; }
+    }
+#pragma warning restore IDE1006 // Naming Styles
+
+    public class BingTranslator
+    {
+        public static string CallTranslate(string input)
+        {
+            Console.WriteLine($"\t\tCalling Bing: {input}");
+            var client = new RestClient("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=zh");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Ocp-Apim-Subscription-Key", "key");
+            request.AddHeader("Content-Type", "application/json");
+
+            var inputSource = new List<Translation>();
+            inputSource.Add(new Translation
+            {
+                Text = input
+            });
+            var inputJson = JsonConvert.SerializeObject(inputSource);
+            request.AddParameter("undefined", inputJson, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            var json = response.Content;
+            var result = JsonConvert.DeserializeObject<List<W>>(json);
+            return result[0].translations[0].text;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CoreTranslator
+{
+    public enum StringType
+    {
+        Tag,
+        Razor,
+        Text
+    }
+    public class StringPart
+    {
+        public StringType StringType { get; set; }
+        public string Content { get; set; }
+        public override string ToString()
+        {
+            return this.Content;
+        }
+    }
+
+    public class TranslatePair
+    {
+        public string SourceString { get; set; }
+        public string TargetString { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            string[] cshtmls = Directory.GetFileSystemEntries(currentDirectory, "*.cshtml", SearchOption.AllDirectories);
+            foreach (var cshtml in cshtmls.Where(t => !t.EndsWith("td.cshtml")))
+            {
+                Console.WriteLine($"Analysing: {cshtml}");
+                var fileName = Path.GetFileName(cshtml);
+                if (fileName.Contains("_ViewStart") || fileName.Contains("_ViewImports"))
+                {
+                    continue;
+                }
+                var file = File.ReadAllText(cshtml);
+                var document = RenderFile(file);
+                var xmlResources = new List<TranslatePair>();
+                Console.WriteLine($"Translating: {cshtml}");
+                for (int i = 0; i < document.Count; i++)
+                {
+                    var textPart = document[i];
+                    if (textPart.StringType == StringType.Text && textPart.Content.Trim() != string.Empty && !textPart.Content.Contains('@'))
+                    {
+                        xmlResources.Add(new TranslatePair
+                        {
+                            SourceString = textPart.Content,
+                            TargetString = BingTranslate(textPart.Content)
+                        });
+                        textPart.Content = Translate(textPart.Content);
+                    }
+                    else if (textPart.StringType == StringType.Tag && textPart.Content.ToLower().StartsWith("<script"))
+                    {
+                        document[i + 1].StringType = StringType.Tag;
+                    }
+                    else if (textPart.StringType == StringType.Tag && textPart.Content.ToLower().StartsWith("<link"))
+                    {
+                        document[i + 1].StringType = StringType.Tag;
+                    }
+                }
+                Console.WriteLine($"Rendering: {cshtml}");
+                var translated = RenderCSHtml(document);
+                var translatedResources = GenerateXML(xmlResources);
+
+
+                var xmlPosition = cshtml.Replace("\\Views\\", "\\Resources\\Views\\").Replace(".cshtml", ".zh.resx");
+                var toWrite = Directory.CreateDirectory(new FileInfo(xmlPosition).Directory.FullName);
+                File.WriteAllText(xmlPosition, translatedResources);
+                File.WriteAllText(cshtml.Replace(".cshtml", ".cshtml"), translated);
+                Console.WriteLine($"Writting: {xmlPosition}");
+            }
+        }
+
+        static List<StringPart> RenderFile(string html)
+        {
+            var document = new List<StringPart>();
+            while (html.Trim().Length > 0)
+            {
+                var (newpart, remainingHtml) = GetNextPart(html);
+                html = remainingHtml;
+                document.Add(newpart);
+            }
+            return document;
+        }
+
+        static string RenderCSHtml(List<StringPart> parts)
+        {
+            string cshtml = "";
+            foreach (var part in parts)
+            {
+                if (part != null)
+                {
+                    cshtml += part.Content;
+                }
+            }
+            return cshtml;
+        }
+
+        static string Translate(string input)
+        {
+            return $"@Localizer[\"{input.Trim()}\"]";
+        }
+
+        static (StringPart, string) GetNextPart(string html)
+        {
+            var part = new StringPart();
+            if (html.Trim().Length < 1)
+            {
+                throw new Exception();
+            }
+            if (html[0] == '<')
+            {
+                part.StringType = StringType.Tag;
+                part.Content = html.Substring(0, html.IndexOf('>') + 1);
+                return (part, html.Substring(html.IndexOf('>') + 1));
+            }
+            else if (html.Trim()[0] == '@' || html.Trim()[0] == '}')
+            {
+                part.StringType = StringType.Razor;
+                var endPoint = html.IndexOf('<');
+                if (endPoint > 0)
+                {
+                    part.Content = html.Substring(0, endPoint);
+                    return (part, html.Substring(endPoint));
+                }
+                else
+                {
+                    part.Content = html;
+                    return (part, "");
+                }
+            }
+            else
+            {
+                part.StringType = StringType.Text;
+                var endPoint = html.IndexOf('<');
+                if (endPoint > 0)
+                {
+                    part.Content = html.Substring(0, endPoint);
+                    return (part, html.Substring(endPoint));
+                }
+                else
+                {
+                    part.Content = html;
+                    return (part, "");
+                }
+            }
+        }
+
+        static string BingTranslate(string input)
+        {
+            return BingTranslator.CallTranslate(input);
+        }
+
+        static string GenerateXML(List<TranslatePair> sourceDocument)
+        {
+            var programPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var generatedItems = string.Empty;
+            foreach (var item in sourceDocument)
+            {
+                generatedItems += $"<data name=\"{item.SourceString.Trim()}\" xml:space=\"preserve\"><value>{item.TargetString.Trim()}</value></data>\r\n";
+            }
+            var template = File.ReadAllText(programPath + "\\Template.xml");
+            var translated = template.Replace("{{Content}}", generatedItems);
+            return translated;
+        }
+    }
+}
